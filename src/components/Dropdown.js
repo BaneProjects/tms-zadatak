318c5ccb423b0cd48988af3a5389194d77d45dfd
Branislav <banea380@gmail.com>
2024-11-14 03:11:53 +0100

    drodown changes

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,8 +1,148 @@
-const Dropdown = ({refInside}) => {
+import Button from "./Button";
+import govermentImg from "../assets/dropdownimages/govermentImg.png"
+  ;
+const Dropdown = ({ refInside }) => {
+
+  const colOne = [
+    {
+      isGroup: true,
+      title: "Professional services",
+    },
+    {
+      title: "Digital businesses",
+      icon: govermentImg,
+    },
+    {
+      title: "Digital businesses",
+      icon: govermentImg,
+    },
+    {
+      title: "Digital businesses",
+      icon: govermentImg,
+    },
+    {
+      title: "Digital businesses",
+      icon: govermentImg,
+    },
+    {
+      title: "Digital businesses",
+      icon: govermentImg,
+    },
+
+    {
+      isGroup: true,
+      title: "Professional services",
+    },
+    {
+      title: "Digital businesses",
+      icon: govermentImg,
+    },
+    {
+      title: "Digital businesses",
+      icon: govermentImg,
+    },
+  ];
+
+  const colTwo = [
+    {
+      isGroup: true,
+      title: "Professional services",
+    },
+    {
+      title: "Digital businesses",
+      icon: govermentImg,
+    },
+    {
+      title: "Digital businesses",
+      icon: govermentImg,
+    },
+    {
+      title: "Digital businesses",
+      icon: govermentImg,
+    },
+    {
+      title: "Digital businesses",
+      icon: govermentImg,
+    },
+
+    {
+      isGroup: true,
+      title: "Professional services",
+    },
+    {
+      title: "Digital businesses",
+      icon: govermentImg,
+    },
+    {
+      title: "Digital businesses",
+      icon: govermentImg,
+    },
+    {
+      title: "Digital businesses",
+      icon: govermentImg,
+    },
+  ];
+
+  const colThree = [
+    {
+      isGroup: true,
+      title: "Professional services",
+    },
+    {
+      title: "Digital businesses",
+      icon: govermentImg,
+    },
+  ];
+
   return (
     <div className="dropdown" ref={refInside}>
-      <div className="dropdown-left"></div>
-      <div className="dropdown-right"></div>
+      <div className="dropdown-left">
+        <div className="dropdown-items">
+          <div className="dropdown-col">
+            {
+              colOne.map((item) => {
+                if (item.isGroup) {
+                  return (<div className="group">{item.title}</div>)
+                } else {
+                  return (<div><img src={item.icon} alt="" />{item.title}</div>)
+                }
+              })
+            }
+          </div>
+          <div className="dropdown-col">
+            {
+              colTwo.map((item) => {
+                if (item.isGroup) {
+                  return (<div className="group">{item.title}</div>)
+                } else {
+                  return (<div><img src={item.icon} alt="" />{item.title}</div>)
+                }
+              })
+            }
+          </div>
+        </div>
+        <div className="buttons">
+          <div>
+            <div><b>Can't find your industry?</b></div>
+            <div>Book a demo to see if we're a match!</div>
+          </div>
+          <Button title="Book a demo" />
+        </div>
+      </div>
+
+      <div className="dropdown-right">
+        <div className="dropdown-col">
+          {
+            colThree.map((item) => {
+              if (item.isGroup) {
+                return (<div className="group">{item.title}</div>)
+              } else {
+                return (<div><img src={item.icon} alt="" />{item.title}</div>)
+              }
+            })
+          }
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,10 +5,10 @@ import caretBlue from "../assets/caret-blue.png";
 const Nav = ({ handleClickCaret }) => {
   return (
     <nav>
-      <div className="nav-item" onClick={handleClickCaret}>Features<span class="caret-white"><img src={caret} /></span></div>
+      <div className="nav-item" onClick={handleClickCaret}>Features<span className="caret-white"><img src={caret} /></span></div>
       <div className="nav-item">Demos</div>
-      <div className="nav-item" onClick={handleClickCaret}>Who’ it for<span class="caret-white"><img src={caret} /></span><span class="caret-blue"><img src={caretBlue} /></span></div>
-      <div className="nav-item" onClick={handleClickCaret}>Resources<span class="caret-white"><img src={caret} /></span></div>
+      <div className="nav-item" onClick={handleClickCaret}>Who’ it for<span className="caret-white"><img src={caret} /></span><span className="caret-blue"><img src={caretBlue} /></span></div>
+      <div className="nav-item" onClick={handleClickCaret}>Resources<span className="caret-white"><img src={caret} /></span></div>
       <div className="nav-item">About us</div>
       <div className="nav-item">Pricing</div>
       <div className="nav-item nav-item--btn mobile-and-tablet">
